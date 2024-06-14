08702a13f1a49da22e2a8202eddca396cc0f78f1
Andrés Martínez <andres.martinez@proveeduria.unam.mx>
2024-06-14 17:40:51 -0600

    Initial Commit

---
+++ Ejercicio 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ejercicio 1
+//
+//  Created by DISMOV on 14/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
